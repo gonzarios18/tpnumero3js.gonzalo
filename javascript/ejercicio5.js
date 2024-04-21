e3120de4f6227c3fb3071cd0cafbe211419ef681
Gonzalo Rios <gonzarios95@gmail.com>
2024-04-21 13:43:54 +0200

    ejercicio 5 incluido

--- javascript/ejercicio5.js
+++ javascript/ejercicio5.js
@@ -1,5 +1,22 @@
 /*Definir una función que muestre información
  sobre una cadena de texto que se le pasa como
-  argumento. A partir de la cadena que se le pasa,
+argumento. A partir de la cadena que se le pasa,
 la función determina si esa cadena está formada
- sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.*/
+sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.*/
+
+function mayusculaoMin(cadena){
+if(cadena=== cadena.toUpperCase()){
+    console.log("totalmente en mayusculas");
+
+} else if(cadena=== cadena.toLowerCase()){
+
+    console.log("totalmente en minusculas");
+} else {
+
+    console.log("mayusculas y minusculas");
+}
+
+}
+
+let cadena= prompt("Ingrese una cadena de texto");
+mayusculaoMin(cadena);
